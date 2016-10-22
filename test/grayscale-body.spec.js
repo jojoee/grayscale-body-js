f70db191034897ba5e09b7cda101b49b5cf26ca2
Nathachai Thongniran <inid3a@gmail.com>
2016-10-22 18:01:25 +0700

    Init

---
+++ test/grayscale-body.spec.js
@@ -0,0 +1,9 @@
+var chai = require('chai'),
+  expect = chai.expect;
+
+describe('Main', function() {
+  it('Mocha and Chai should work', function() {
+    expect(true).to.be.true;
+    expect(false).to.be.false;
+  });
+});
